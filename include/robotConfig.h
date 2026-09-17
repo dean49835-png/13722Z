5b4523614d104c3b3384cf4b81346860cd99d252
unknown <dean49835@gmail.com>
2026-09-17 18:39:34 -0400

    Updated robot configuration and LemLib settings

---
+++ include/robotConfig.h
@@ -0,0 +1,20 @@
+#ifndef _PROS_ROBOT_CONFIG_H_
+#define _PROS_ROBOT_CONFIGH_
+
+#include "api.h"
+#include "lemlib/api.hpp"
+
+//drivetrain stuffs
+extern pros::MotorGroup left_mortor_groups;
+extern pros::MotorGroup right_motor_groups; 
+extern lemlib::Chassis chassis;
+
+//bot motors
+
+extern pros::Motor intake;
+extern pros::Motor cascade;
+
+
+
+#endif
+
